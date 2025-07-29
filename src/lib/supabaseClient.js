ab0e25ea237ce7962ac0459a16ead549224f5f76
RaresTecno <rarestecno@gmail.com>
2025-07-29 20:25:45 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Captura la URL y la clave pública (anon key) desde variables de entorno
-const supabaseUrl = "https://lrrpcznutedkorbkvfre.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycnBjem51dGVka29yYmt2ZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0MzQzMzUsImV4cCI6MjA2OTAxMDMzNX0.cndyXsMOuIOfKOJQ5BDY1W_9UmJKROLouAeKXd_YCOc";
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 // Verifica que existan las variables necesarias
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Faltan las variables de entorno NEXT_PUBLIC_SUPABASE_URL o NEXT_PUBLIC_SUPABASE_ANON_KEY');
